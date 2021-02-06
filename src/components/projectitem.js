299d9093e8baf086d4240681511b39db9af78d97
John Hauth <jkhauth@gmail.com>
2021-02-06 15:14:26 -0500

    project database set up and implemented

--- src/components/projectitem.js
+++ src/components/projectitem.js
@@ -1,19 +1,25 @@
+import { logDOM } from '@testing-library/react';
 import React from 'react';
-
-
+// import dmmlogo from "../utlis/images/dmm-logo.png"
+// src\utlis\images\dmm-logo.png
 const ProjectItem = ({project}) => {
 
 return (
 
-<div className="d-flex mb-2 flex-column align-items-center justify-content-center projectdiv">
+<div className="d-flex mb-3 flex-column align-items-center justify-content-center projectdiv mx-3 p-2">
+    <div className="d-flex flex-row flex-wrap justify-content-center align-items-center">
+    <img className="img-fluid projectimg mx-2" src={project.imagesrc} alt="project-pic"></img>
+    <div className="d-flex flex-column align-items-center justify-content-center">
     <h2 className="text-center sectionheader">{project.projecttitle}</h2>
-    <div className="d-flex flex-row flex-wrap">
-    <img src="#" alt="project-pic"></img>
-    <p className="text-center">{project.projectdescription}</p>
+    <div className="d-flex projectdescription">
+    <p className="text-justify mb-0">{project.projectdescription}</p>
     </div>
-    <h4>{project.languages}</h4>
+    <h4 className="text-center">Languages used:</h4>
+    <h4 className="text-center">{project.languages}</h4>
     <a href={project.applicationurl}>Application Url</a>
     <a href={project.githuburl}>GitHub Url</a>
+    </div>
+    </div>
 </div>
 
  )}
